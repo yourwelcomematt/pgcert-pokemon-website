993ee67a6b9fa650e3905af5ee85d5dda47b4ab9
mattding <mdin874@aucklanduni.ac.nz>
2021-04-24 17:56:30 +1200

    Created Part 2

---
+++ ictgradschool/web/assignment/part2/pokemon.js
@@ -0,0 +1,10 @@
+/* Base address for the Pokemon endpoints. Add the endpoint name and parameters onto this */
+const ENDPOINT_BASE_URL = "https://trex-sandwich.com/pokesignment/";
+
+
+/* TODO: Your code here */
+window.addEventListener("load", function () {
+
+
+
+});
