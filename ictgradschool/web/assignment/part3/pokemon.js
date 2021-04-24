105ef25604698f34e15167e972171c2b1e6140b5
mattding <mdin874@aucklanduni.ac.nz>
2021-04-24 21:32:10 +1200

    Completed displayPokemonDetails function

--- ictgradschool/web/assignment/part3/pokemon.js
+++ ictgradschool/web/assignment/part3/pokemon.js
@@ -10,7 +10,17 @@ window.addEventListener("load", function () {
     displayPokemonOfTheDay();
 
     pokemonOfTheDayPanel.addEventListener("click", refreshPokemonOfTheDay); 
+
+    const pokemonDetailsPanel = document.querySelector("#detailsPane");
+
+    displayPokemonDetails();
+
     
+
+
+
+
+
     async function getPokemonOfTheDay() {
         const response = await fetch(`https://trex-sandwich.com/pokesignment/pokemon?random=random`);
         const responseObject = await response.json();
@@ -49,6 +59,44 @@ window.addEventListener("load", function () {
         nameElement.remove();
         descriptionElement.remove();
         displayPokemonOfTheDay();
+    };
+
+    async function displayPokemonDetails() {
+        const pokemonArrayResponse = await fetch(`https://trex-sandwich.com/pokesignment/pokemon`);
+        const pokemonArray = await pokemonArrayResponse.json();
+        for (let i = 0; i < pokemonArray.length; i++ ) {
+            const pokemon = pokemonArray[i];
+            const pokemonAddress = "https://trex-sandwich.com/pokesignment/pokemon?pokemon=" + pokemon;
+            const pokemonResponseObject = await fetch(pokemonAddress);
+            const pokemonObject = await pokemonResponseObject.json();
+
+            const pokemonPanel = document.createElement("div");
+            pokemonPanel.style.backgroundColor = "#46ACC2"
+            pokemonDetailsPanel.appendChild(pokemonPanel);
+            pokemonPanel.style.margin = "50px";
+            pokemonPanel.addEventListener("click", displayClickedPokemonDetails);
+
+            const pokemonPanelPictureElement = document.createElement("img");
+            const pokemonPanelPictureAddress = "https://trex-sandwich.com/pokesignment/img/" + pokemonObject.image;
+            pokemonPanelPictureElement.src = pokemonPanelPictureAddress;
+            pokemonPanel.appendChild(pokemonPanelPictureElement);
+
+            const pokemonPanelNameElement = document.createElement("h2");
+            const pokemonPanelName = pokemonObject.name;
+            pokemonPanelNameElement.innerHTML = `<strong>${pokemonPanelName}</strong>`;
+            pokemonPanel.appendChild(pokemonPanelNameElement);
+            pokemonPanelNameElement.style.fontSize = "medium";
+            pokemonPanelNameElement.style.paddingBottom = "35px";
+        };
+    };
+
+    async function displayClickedPokemonDetails () {
+        const pokemonDetailsPanelArray = document.querySelectorAll("#detailsPane > div");
+        for (i = 0; i < pokemonDetailsPanelArray.length; i++) {
+            pokemonDetailsPanelArray[i].remove();
+        }
+
+        
     };
     
 });
